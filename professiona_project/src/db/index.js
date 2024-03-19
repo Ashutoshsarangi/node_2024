b8c6a103b7867da0bd0e6a5ae7f6bd8c60a11ee7
Ashu <ashutoshsarangi95@gmail.com>
2024-03-19 16:18:29 +0200

    Removed console.log.

--- professiona_project/src/db/index.js
+++ professiona_project/src/db/index.js
@@ -4,7 +4,6 @@ import { DB_NAME } from '../constants.js';
 export const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`);
-        console.log(connectionInstance.connections);
         console.log(`MONGO DB Connected Sucessfully On Host:- ${connectionInstance.connections[0].host} Port:- ${connectionInstance.connections[0].port}`);
 
     } catch (error) {
